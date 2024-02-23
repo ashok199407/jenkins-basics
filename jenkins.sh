80851e3d608e39d732af368fd8b8df98959a0f6a
ashok199407 <ashok01399.marolix@gmail.com>
2024-02-23 11:16:19 +0530

    first commit

---
+++ jenkins.sh
@@ -0,0 +1,14 @@
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+#sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo dnf install -y fontconfig java-17-openjdk
+sudo dnf install -y jenkins
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+jenkins --version
+java --version
+exit
+exit
